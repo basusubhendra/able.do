03c1785e8dc2c69d770909fa970e29b03b00b1dc
Subhendra Basu <basu.subhendra@outlook.com>
2022-07-08 16:45:47 +0530

    Converted to a object oriented specification

--- generate_combinations.cpp
+++ generate_combinations.cpp
@@ -4,8 +4,30 @@
 #include <iostream>
 #include <boost/property_tree/json_parser.hpp>
 
-void print_contents( const boost::property_tree::ptree& pt)
-{
+class JSON_Obj {
+	protected:
+		char* json_file;
+		boost::property_tree::ptree pt;
+	public:
+		JSON_Obj(char* f);
+		void upload_contents();
+		boost::property_tree::ptree& getPropertyTree();
+		void print_contents(const boost::property_tree::ptree&);
+};
+
+JSON_Obj::JSON_Obj(char* f) {
+	this->json_file = f;
+}
+
+void JSON_Obj::upload_contents() {
+	boost::property_tree::read_json(json_file, pt);
+}
+
+boost::property_tree::ptree& JSON_Obj::getPropertyTree() {
+	return this->pt;
+}
+
+void JSON_Obj::print_contents(const boost::property_tree::ptree& pt) {
     using boost::property_tree::ptree;
     for (const auto& x: pt )
     {
@@ -15,10 +37,8 @@ void print_contents( const boost::property_tree::ptree& pt)
 }
 
 int main(int argc, char* argv[]) {
-	char* json_file = strdup(argv[1]);
-	printf("\nJSON FILE being read is %s\n", json_file);
-	boost::property_tree::ptree pt;
-	boost::property_tree::read_json(json_file, pt);
-	print_contents(pt);
+	JSON_Obj* main_obj = new JSON_Obj(argv[1]);
+	main_obj->upload_contents();
+	main_obj->print_contents(main_obj->getPropertyTree());
 	return 0;
 }
