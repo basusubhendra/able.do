32adcc5a35fba280a6aec65fa2ab75620510e8bc
Subhendra Basu <basu.subhendra@outlook.com>
2022-07-08 20:41:19 +0530

    Traversing the JSON Tree

--- generate_combinations.cpp
+++ generate_combinations.cpp
@@ -1,8 +1,15 @@
+#include <string>
+#include <set>
+#include <exception>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <iostream>
 #include <boost/property_tree/json_parser.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/foreach.hpp>
+using namespace std;
+using boost::property_tree::ptree;
 
 class JSON_Obj {
 	protected:
@@ -12,7 +19,6 @@ class JSON_Obj {
 		JSON_Obj(char* f);
 		void upload_contents();
 		boost::property_tree::ptree& getPropertyTree();
-		void print_contents(const boost::property_tree::ptree&);
 };
 
 JSON_Obj::JSON_Obj(char* f) {
@@ -27,18 +33,21 @@ boost::property_tree::ptree& JSON_Obj::getPropertyTree() {
 	return this->pt;
 }
 
-void JSON_Obj::print_contents(const boost::property_tree::ptree& pt) {
-    using boost::property_tree::ptree;
-    for (const auto& x: pt )
-    {
-        std::cout << x.first << ": " << x.second.get_value<std::string>() << std::endl;
-        print_contents(x.second);
-    }
+void print_contents(const boost::property_tree::ptree& pt) {
+	using boost::property_tree::ptree;
+	for (const auto& x: pt ) {
+		std::cout << x.first << ": " << x.second.get_value<std::string>() << std::endl;
+		print_contents(x.second);
+	}
 }
 
 int main(int argc, char* argv[]) {
 	JSON_Obj* main_obj = new JSON_Obj(argv[1]);
 	main_obj->upload_contents();
-	main_obj->print_contents(main_obj->getPropertyTree());
+	boost::property_tree::ptree pt = main_obj->getPropertyTree();
+	boost::property_tree::ptree attribute_node = pt.get_child("attributes");
+	boost::property_tree::ptree value_node = attribute_node.get_child("values");
+	BOOST_FOREACH(boost::property_tree::ptree::value_type const& v, value_node) {
+	}
 	return 0;
 }
