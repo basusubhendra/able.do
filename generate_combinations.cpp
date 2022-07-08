6cbfe8a626c02c380b3f4bb337bf4e6153e5faa6
Subhendra Basu <basu.subhendra@outlook.com>
2022-07-08 16:33:21 +0530

    Able to read JSON file

--- generate_combinations.cpp
+++ generate_combinations.cpp
@@ -1,11 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <iostream>
+#include <boost/property_tree/json_parser.hpp>
+
+void print_contents( const boost::property_tree::ptree& pt)
+{
+    using boost::property_tree::ptree;
+    for (const auto& x: pt )
+    {
+        std::cout << x.first << ": " << x.second.get_value<std::string>() << std::endl;
+        print_contents(x.second);
+    }
+}
 
 int main(int argc, char* argv[]) {
 	char* json_file = strdup(argv[1]);
 	printf("\nJSON FILE being read is %s\n", json_file);
-	FILE* fjson = fopen64(json_file, "r");
-	fclose(fjson);
+	boost::property_tree::ptree pt;
+	boost::property_tree::read_json(json_file, pt);
+	print_contents(pt);
 	return 0;
 }
